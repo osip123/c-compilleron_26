4c9b4f5d6357fe02bd06903b079629b99a368c85
osip123 <131646261+osip123@users.noreply.github.com>
2024-07-02 23:37:03 +0300

    lexer part 3

---
+++ src/err/err.cpp
@@ -0,0 +1,5 @@
+//
+// Created by endoc on 02.07.2024.
+//
+
+#include "err.h"
---
+++ src/err/err.h
@@ -0,0 +1,14 @@
+//
+// Created by endoc on 02.07.2024.
+//
+
+#ifndef C__COMPILLERON_26_ERR_H
+#define C__COMPILLERON_26_ERR_H
+
+
+class err {
+
+};
+
+
+#endif //C__COMPILLERON_26_ERR_H
